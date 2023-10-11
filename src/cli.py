198a24a27bcde0036267c141d69dde7cc49f24ad
Benjamin Chiang <chiangbenjamin991@gmail.com>
2023-10-11 07:04:46 -0700

    feat: create basic cli module

---
+++ src/cli.py
@@ -0,0 +1,24 @@
+"""
+This entrypoint module is responsible for parsing command line arguments and applying setting overrides.
+"""
+
+import argparse
+from Game import Game
+
+CLI_DESC = "Initialize the snake game."
+
+
+def parse():
+    """
+    Parse command line arguments and apply setting overrides.
+    """
+    parser = argparse.ArgumentParser(description=CLI_DESC)
+    parser.add_argument("--tickrate", type=str, default="64", help="The number of times to update the game per second.")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse()
+    game = Game(tickrate=int(args.tickrate))
+    game.start()
