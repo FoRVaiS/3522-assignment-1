ef3ba5ba7abfbd4cd5c2292ad9a469cefed0d0af
Benjamin Chiang <chiangbenjamin991@gmail.com>
2023-10-14 21:25:28 -0700

    feat(components): create render and position components

---
+++ src/Component.py
@@ -0,0 +1,35 @@
+from abc import ABC
+
+import pygame
+
+
+class Component(ABC):
+    pass
+
+
+class RenderComponent(Component):
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+
+    def draw(self, screen: pygame.Surface, x: int, y: int) -> None:
+        square_rect = pygame.Rect(x, y, self.width, self.height)
+
+        # Draw the square on the screen with the specified color
+        pygame.draw.rect(screen, (255, 255, 255), square_rect)
+
+
+class PositionComponent(Component):
+    def __init__(self, x: int, y: int) -> None:
+        self.set_position(x, y)
+
+    def move(self, x: int, y: int) -> None:
+        self.x += x
+        self.y += y
+
+    def set_position(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+    def get_position(self) -> tuple[int, int]:
+        return (self.x, self.y)
